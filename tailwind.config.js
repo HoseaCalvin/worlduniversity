b7ce540c6ba3fbd9f0dcbf45db22f57da9923cbd
HoseaCalvin <hoseacalvin1702@gmail.com>
2025-06-28 13:43:59 +0700

    changed configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,6 @@ export default {
       backgroundColor: {
         'deep-blue': '#1E3A8A',
         'bright-cyan': '#38BDF8',
-      },
-      screens: {
-        '3xs': '320px',
-        '2xs': '375px',
-        'xs': '425px'
       }
     },
   },
